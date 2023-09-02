d9cbdd575e8df8428281d6f73380a0c5cf99554c
Manish pratap  Singh <143782334+Manishpratapsingh23@users.noreply.github.com>
2023-09-02 22:15:36 +0530

    Add files via upload

---
+++ distance.py
@@ -0,0 +1,8 @@
+#distance between two coordinate points(x1,y1) and(x2,y2)
+x1=int(input("enter x1 coordinates: "))
+y1=int(input("enter y1 coordinates: "))
+x2=int(input("enter x2 coordinates: "))
+y2=int(input("enter y2 coordinates: "))
+d=((x2-x1)**2+(y2-y1)**2)**(1/2)
+print("the distance is ",d)
+
