3f42ba672c6fa1cbf05d56eabeb2b2338d39695f
Manish pratap  Singh <143782334+Manishpratapsingh23@users.noreply.github.com>
2023-09-02 15:47:45 +0530

    Add files via upload

---
+++ sum1.py
@@ -0,0 +1,4 @@
+#sum of first n natural number
+n=int(input("enter limit: "))
+s=(n*(n+1))/2
+print("sum of first ",n," natural number is: ",s)
---
+++ swap.py
@@ -0,0 +1,7 @@
+#swap numbers
+a=int(input("enter first number: "))
+b=int(input("enter second number: "))
+t=a
+a=b
+b=t
+print("swapped values are: ",a," , ",b)
