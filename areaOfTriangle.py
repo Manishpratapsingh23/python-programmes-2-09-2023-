a9bebc93754af3e642e1eeddd4ad23ebfdadd02e
Manish pratap  Singh <143782334+Manishpratapsingh23@users.noreply.github.com>
2023-09-02 21:35:54 +0530

    Add files via upload

---
+++ areaOfTriangle.py
@@ -0,0 +1,5 @@
+#area of triangle usinag base and height
+b=int(input("enter base of triangle: "))
+h=int(input("enter height of triangle: "))
+area=(b*h)/2
+print("area of triangle: ",area)
---
+++ volumeOfSphere.py
@@ -0,0 +1,4 @@
+#volume of sphere
+r=float(input("enter radius of sphere: "))
+v=(4*3.14*r*r*r)/3
+print("volume of sphere is ",v)
